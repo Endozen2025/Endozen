411bb6ca667abcdb2718fb2c2128160fb48c0299
Solutionsnextwave <gogulan@solutionsnext.in>
2025-07-26 10:15:06 +0530

    feat: scroll to top on route change for better navigation UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,15 +12,16 @@ import Contact from './pages/Contact';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
 import CMS from './pages/CMS';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-white">
         <Routes>
           {/* CMS Route - No Header/Footer for clean admin interface */}
           <Route path="/cms" element={<CMS />} />
-          
           {/* Main Website Routes - With Header/Footer */}
           <Route path="/*" element={
             <>
